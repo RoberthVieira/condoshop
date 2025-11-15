1e89216ab2e579973a39f2846336398917dd987e
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-11-14 23:21:57 -0300

    Update user passwords in users.ts

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,8 +1,5 @@
 import type { User } from "../types/userTypes";
 
 export const users: User[] = [
-  { id: 1, name: "Roberth", password: "A997760050a@" },
-  { id: 2, name: "Maria", password: "123456" },
-  { id: 3, name: "Joao", password: "senha123" },
-  { id: 4, name: "Brenda", password: "12345678"}
+  { id: 1, name: "Roberth", password: "123456789" }
 ]
