7f1c3c37fe85c31c0114413e2084ababe4e0f3d7
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-11-17 22:50:31 -0300

    exclui item da lista de usuarios

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,10 +1,5 @@
 import type { User } from "../types/userTypes";
 
 export const users: User[] = [
-<<<<<<< HEAD
   { id: 1, name: "Roberth", password: "123456789" }
 ]
-=======
-  { id: 1, name: "Roberth", password: "A997760050a@" },
-]
->>>>>>> 72cc561 (modifica arquivo Readme.md)
