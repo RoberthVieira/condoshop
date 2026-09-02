11e97d994f968218e90aac04920ef2e4f0a19220
roberth <roberthvieirasant@gmail.com>
2026-09-02 15:18:54 -0300

    fix: limpa carrinho somente apos confirmacao do pagamento na pagina de sucesso

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { CarrinhoProvider } from './context/CarrinhoContext.tsx'
 import './style/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <CarrinhoProvider>
       <App />
+    </CarrinhoProvider>
   </BrowserRouter>
 )
